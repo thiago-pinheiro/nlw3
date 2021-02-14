9b8576c948eaf669c98743d8e10bb6247a567e44
Thiago Pinheiro <dev.thiagopinheiro@gmail.com>
2021-02-14 15:21:41 -0300

    Iniciando o Back-end

---
+++ src/server.js
@@ -0,0 +1,17 @@
+//importar dependencias
+const express = require('express');
+const path = require('path');
+
+//iniciando o express
+const server = express();
+server
+//utilizando os arquivos estáticos
+.use(express.static('public'))
+
+//criar uma rota
+server.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+//ligar o servidor 
+server.listen(5500)
