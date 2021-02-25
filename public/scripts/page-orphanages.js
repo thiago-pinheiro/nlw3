d982225dfd339a4e5da52a467258aba5bc8178e9
Thiago Pinheiro <dev.thiagopinheiro@gmail.com>
2021-02-25 19:38:31 -0300

    Quase finalizando o Back-end

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -20,16 +20,34 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
+function addMarker({id, name, lat, lng}){
+
+    
 //create popup overlay
 const popup = L.popup({
     closeButton: false,
     className: "map-popup",
     minWidth: 240,
     minHeight: 240,
-}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg" </a>')
+}).setContent(`${name}  <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg" </a>`)
 
 
-L.marker([-3.7679247,-38.5311203], {icon})
+L.marker([lat,lng], {icon})
     .addTo(map)
     .bindPopup(popup)
     
+
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+
+    }
+
+    addMarker(orphanage)
+})
